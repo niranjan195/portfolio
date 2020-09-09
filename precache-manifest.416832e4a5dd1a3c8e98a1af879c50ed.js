59c239e2b1b100ebba017f614c6c23d5ac8d313a
niranjan hegde <hegdeniranjan144@gmail.com>
2020-09-09 20:50:00 +0530

    Updates

---
+++ precache-manifest.416832e4a5dd1a3c8e98a1af879c50ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6fe6272fdf16065e1d3f5b88fa82a98",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a459e318492dfd5d4fc0",
+    "url": "/portfolio/static/css/main.dbc16fcd.chunk.css"
+  },
+  {
+    "revision": "6b8d7ad0a22d5109c41e",
+    "url": "/portfolio/static/js/2.d64dbdd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.d64dbdd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a459e318492dfd5d4fc0",
+    "url": "/portfolio/static/js/main.163b12a9.chunk.js"
+  },
+  {
+    "revision": "309a9e55c61ba5bff665",
+    "url": "/portfolio/static/js/runtime-main.b3df5f95.js"
+  },
+  {
+    "revision": "efdba04681a3215e786c0e996e4bc780",
+    "url": "/portfolio/static/media/home.efdba046.jpg"
+  },
+  {
+    "revision": "b1fecd9c70421345e9afcc41eaa32407",
+    "url": "/portfolio/static/media/profile3.b1fecd9c.png"
+  }
+]);
