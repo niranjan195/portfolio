cdd7ae0dc525d305360389aebd0cb9a119b66293
Niranjan Hegde <niranjan.hegde@niki.ai>
2021-06-30 21:34:13 +0530

    Updates

--- precache-manifest.81f94e74b2709f0d71c6f8c143c06e89.js
+++ precache-manifest.81f94e74b2709f0d71c6f8c143c06e89.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6b0a4689919775b0871886ade89d1135",
+    "revision": "7a3326cb69cda2418420ea142e4c331a",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "ffb500e83442e576b2aa",
-    "url": "/portfolio/static/css/main.a630be29.chunk.css"
+    "revision": "16d6accd436c565025bb",
+    "url": "/portfolio/static/css/main.a9042dd1.chunk.css"
   },
   {
-    "revision": "8972fd6bd4cae6b917c0",
-    "url": "/portfolio/static/js/2.e4de6411.chunk.js"
+    "revision": "7ff605e31677a82918f9",
+    "url": "/portfolio/static/js/2.0390f709.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/portfolio/static/js/2.e4de6411.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.0390f709.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ffb500e83442e576b2aa",
-    "url": "/portfolio/static/js/main.f5c0ab32.chunk.js"
+    "revision": "16d6accd436c565025bb",
+    "url": "/portfolio/static/js/main.7d8cfa3a.chunk.js"
   },
   {
     "revision": "309a9e55c61ba5bff665",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.458d256179e32bb7200175de9247564b.js"
+  "/portfolio/precache-manifest.81f94e74b2709f0d71c6f8c143c06e89.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
